163fe8f87b99394e57c6af3fd0d1c63a4bacf616
tylerdoreilly <tylerdoreilly@gmail.com>
2024-02-20 01:29:39 -0500

    add basic cors config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { google } = require('googleapis');
 const app = express();
+app.use(cors());
 const { envPort, env, spreadsheetId } = require('./config');
 const port = process.env.PORT || envPort;
 const utility = require('./utilities/formatText');
